fcbb3a8cd94163f01a8790e93a9187d35bc63b46
andriy <and.feduniv@gmail.com>
2016-02-19 12:54:20 +0200

    Change directory

--- UnitTest/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=utest',
-    'username' => 'root',
-    'password' => 'TryWqe112',
-    'charset' => 'utf8',
-];
